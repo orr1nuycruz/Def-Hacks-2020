d7eac0fed54fb0d034b75b7f9cd608fbee1e5d06
Ifavhoney <nuthsaid@outlook.com>
2020-06-21 19:31:01 -0400

    save changes

--- routes/index.js
+++ routes/index.js
@@ -32,4 +32,32 @@ router.get('/:type/:fileName/:token', async function (req, res, next) {
 
 
 });
+
+
+router.get('/:type/:fileName/:token', async function (req, res, next) {
+
+  var url = "https://firebasestorage.googleapis.com/v0/b/devhacks2020-1cf35.appspot.com/o/" +
+    req.params.type + "%2F" + req.params.fileName
+    + "?alt=media&token" + req.params.token;
+
+  console.log(url + "");
+
+  const [result] = await client.textDetection(url)
+  const detections = result.textAnnotations;
+  console.log(detections[0].locale)
+  if (detections[0].locale != null) {
+    res.json({ "data": "success", "text": detections[0] });
+
+  }
+  else {
+    res.json({
+      "data": "failure", "text": "null"
+    });
+
+  }
+
+
+});
+
+
 module.exports = router;

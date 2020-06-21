e65fb6de46a586dac364a95043e06ff9f83ce309
Ifavhoney <nuthsaid@outlook.com>
2020-06-21 16:52:44 -0400

    pushing google cloud service

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+require('dotenv').config();
 
+// Import the Google Cloud Vision library
+const vision = require('@google-cloud/vision');
+
+// Create a client
+const client = new vision.ImageAnnotatorClient();
+router.get('/:type/:fileName/:token', async function (req, res, next) {
+
+  var url = "https://firebasestorage.googleapis.com/v0/b/devhacks2020-1cf35.appspot.com/o/" + req.params.type + "%2F" + req.params.fileName + "?alt=media&token" + req.params.token;
+  console.log(url + "");
+
+  const [result] = await client.textDetection(url)
+  const detections = result.textAnnotations;
+  console.log(detections[0].locale)
+  if (detections[0].locale != null) {
+    res.json({ "data": "success", "text": detections[0] });
+
+  }
+  else {
+    res.json({
+      "data": "failure", "text": "null"
+    });
+
+  }
+
+
+});
 module.exports = router;
